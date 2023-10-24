3f236f68e2fdeed34d9622308c2b6da2a41ac803
Ibrahim Hassan <hassan.ibb120044@gmail.com>
2023-10-24 13:41:34 +0100

    Add files via upload

--- javascript/main.js
+++ javascript/main.js
@@ -1,40 +1,25 @@
-let btn = document.querySelectorAll(".num-btn");
+
 let allBtn = document.querySelectorAll(".button");
 let resultBtn = document.querySelector("#result-box");
-let clearBtn = document.querySelector("#clear");
-let total = document.querySelector("#total");
-
-let btnSpread = [...btn];
+let operators = ['+', '-', '%', '/', '*', '=']
 let allBtnSpread = [...allBtn];
+let  result = '';
 
 // for number inputs
-
-btnSpread.forEach((button, i) => {
-    button.addEventListener("click",()=> {
-// inner values
-if (resultBtn.innerHTML== '0'){
-    resultBtn.innerHTML ="";
-}
-let value = btn[i].innerHTML;
-resultBtn.innerHTML += value;
-    });
-});
+allBtnSpread.forEach((button) => 
+    button.addEventListener("click", (e) => evaluate(e.currentTarget.dataset.name))
+);
 
 // evaluate function
+function evaluate(outputValues) {
+   if(outputValues === '=' && result !==''){
+    result = eval(result.replace('%', '/100'));
+   }else if(outputValues === 'AC'){
+    result = '';
+   }else{
+    if(result === '' && operators.includes(outputValues)) return;
+    result += outputValues;
+   };
 
-function evaluate (fn){
-    return new Function("return" + fn)()
-}
-
-// to calculate all function
-
-total.addEventListener ("click", () => {
-    let allInputs = resultBtn.innerHTML;
-    resultBtn.innerHTML = evaluate(allInputs);
-    console.log(evaluate(allInputs));
-});
-
-// clear inputs
-clearBtn.addEventListener("click", () => {
-    resultBtn.innerHTML = "0"
-});
+   resultBtn.innerHTML = result;
+};
